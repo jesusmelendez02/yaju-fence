e3d5fa9713b3cecba6905118cb388fd655b29ac2
jesusmelendez02 <jesusmelendez0212@gmail.com>
2024-10-22 00:43:24 +0200

    First Commit

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// nav 
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
 
-setupCounter(document.querySelector('#counter'))
+navToggle.addEventListener("click", () => {
+    const visibility = nav.getAttribute("data-visible");
+    if (visibility === "false") {
+        nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true)
+    } else {
+            nav.setAttribute("data-visible", false);
+            navToggle.setAttribute("aria-expanded", false)
+        }
+    })
