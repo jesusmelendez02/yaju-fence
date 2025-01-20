fe7bf5d17acb32e52b0d2408bbe9bf35e498831c
jesusmelendez02 <jesusmelendez0212@gmail.com>
2025-01-20 17:50:12 +0100

    Ready for deployment

--- main.js
+++ main.js
@@ -1,32 +1,5 @@
 // nav 
 
-// const nav = document.querySelector(".primary-navigation");
-// const navList = document.querySelectorAll("primary-navigation a");
-// const navToggle = document.querySelector(".mobile-nav-toggle");
-
-// navToggle.addEventListener("click", () => {
-//     const visibility = nav.getAttribute("data-visible");
-//     if (visibility === "false") {
-//         nav.setAttribute("data-visible", true);
-//         navToggle.setAttribute("aria-expanded", true);
-//         document.body.classList.toggle("no-scroll");
-//     } else {
-//             nav.setAttribute("data-visible", false);
-//             navToggle.setAttribute("aria-expanded", false);
-//             document.body.classList.remove("no-scroll");
-//         }
-
-//     });
-
-//     navList.forEach(link => {
-//         link.addEventListener("click", () => {
-//             nav.setAttribute("data-visible", "false");
-//         navToggle.setAttribute("aria-expanded", "false");
-//         document.body.classList.remove("no-scroll");
-//         });
-//     });
-
-
 const nav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const navLinks = document.querySelectorAll(".primary-navigation a"); // All links in the nav
@@ -52,18 +25,7 @@ navLinks.forEach(link => {
 });
 
 
-// modal 
-// const modal = document.querySelector("#modal");
-// const openModal = document.querySelector(".open-button");
-// const closeModal = document.querySelector(".close-button");
-
-// openModal.addEventListener("click", () => {
-//   modal.showModal();
-// });
-
-// closeModal.addEventListener("click", () => {
-//   modal.close();
-// });
+// Modal 
 
 const openModal = document.querySelectorAll(".open-button");
 const modals = document.querySelectorAll(".modal")
